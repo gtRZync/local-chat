8f4cb3db181002ba25d9fc3aeee2827b3b0add28
Myson Dio <sendorfbg@gmail.com>
2025-12-09 03:48:26 -0300

    feat: adding paths config

---
+++ local_chat/config/__init__.py
@@ -0,0 +1,9 @@
+from .path import ASSETS_DIR, SOUND_DIR, DATA_DIR, FONT_DIR, DATABASE_DIR
+
+__all__ = [
+    'ASSETS_DIR',
+    'SOUND_DIR',
+    'DATA_DIR',
+    'FONT_DIR',
+    'DATABASE_DIR'
+]
