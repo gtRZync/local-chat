e136bd870ed142ece4bb00cf5ca5a070fb110535
Myson Dio <sendorfbg@gmail.com>
2025-12-09 03:53:02 -0300

    meeh

---
+++ local_chat/gui/__init__.py
@@ -0,0 +1,9 @@
+from .widgets.app_icon import AppIcon
+from .widgets.status_bar import StatusBar
+from ..utils.time_updatable import TimeUpdatableMixin
+
+__all__ = [
+    'StatusBar',
+    'AppIcon',
+    'TimeUpdatableMixin'
+]
