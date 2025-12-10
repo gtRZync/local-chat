d3dea82407f7b9313f2d763aba1cf9904b5f0820
Myson Dio <sendorfbg@gmail.com>
2025-12-10 04:11:01 -0300

    feat: adding DEFAULT_DATABASE_STRUCTURE to use if database.json is empty

--- local_chat/config/path.py
+++ local_chat/config/path.py
@@ -1,8 +1,12 @@
 from pathlib import Path
 
-# __Traversal     =   Path('../../')
 ASSETS_DIR      =   Path('assets')
 DATA_DIR        =   Path('data')
 DATABASE_DIR    =   DATA_DIR / 'database.json'
 SOUND_DIR       =   ASSETS_DIR / 'sound'
 FONT_DIR        =   ASSETS_DIR / 'font'
+
+DEFAULT_DATABASE_STRUCTURE = {
+  "users": [],
+  "conversations": []
+}
